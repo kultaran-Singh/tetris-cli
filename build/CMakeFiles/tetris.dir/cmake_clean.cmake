fb3ae212696b30e7ff14e428706edfa26720b80e
Kultaran <kultaran99@proton.me>
2025-11-15 16:49:41 +0530

    Initial Commit

---
+++ build/CMakeFiles/tetris.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tetris.dir/link.d"
+  "CMakeFiles/tetris.dir/src/board.cpp.o"
+  "CMakeFiles/tetris.dir/src/board.cpp.o.d"
+  "CMakeFiles/tetris.dir/src/main.cpp.o"
+  "CMakeFiles/tetris.dir/src/main.cpp.o.d"
+  "CMakeFiles/tetris.dir/src/piece.cpp.o"
+  "CMakeFiles/tetris.dir/src/piece.cpp.o.d"
+  "tetris"
+  "tetris.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
